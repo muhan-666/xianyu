5828924770020b49765c385cca955993b3f1b061
mh666 <2330884874@qq.com>
2019-12-28 22:56:39 +0800

    .

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <!-- 幻灯片 -->
+    <el-carousel :interval="5000" arrow="always" trigger="click">
+      <el-carousel-item v-for="(item, index) in banners" :key="index" >
+        <div
+          class="banner-image"
+          :style="`
+                background:url('${$axios.defaults.baseURL + item.url}') center center no-repeat;
+                background-size:contain contain;
+                `"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            // 轮播图数据
+            banners: []
+        }
+    },
+    mounted () {
+      this.$axios({
+          url: '/scenics/banners'
+      }).then(res =>{
+          this.banners = res.data.data
+          console.log(res)
+      })
+    }
+}
+</script>
+
+<style scoped lang="less">
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
+
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
+}
+</style>
---
+++ pages/tab.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="header">
+      <span
+        v-for="(item,index) in ['新闻','体育','娱乐']"
+        :key="index"
+        style="margin:5px;"
+        @click="switchtab(index)"
+        :class="{active:index===current}"
+      >{{item}}</span>
+    </div>
+    <div class="content">
+      <div class="news" v-if="current === 0">
+        <ul>
+          <li>新闻1</li>
+          <li>新闻2</li>
+          <li>新闻3</li>
+        </ul>
+      </div>
+      <div class="tiyu" v-if="current === 1">
+        <ul>
+          <li>体育1</li>
+          <li>体育2</li>
+          <li>体育3</li>
+        </ul>
+      </div>
+      <div class="yule" v-if="current === 2">
+        <ul>
+          <li>娱乐1</li>
+          <li>娱乐2</li>
+          <li>娱乐3</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+data () {
+    return {
+        current: 0
+    }
+},
+methods: {
+    switchtab(index){
+        this.current = index
+    }
+}
+}
+</script>
+
+<style lang="less" scoped>
+.active{
+    color: #f00;
+}
+.news{
+    background-color: #f00;
+}
+.tiyu{
+    background-color: #0f0;
+}
+.yule{
+    background-color: #00f;
+}
+</style>
